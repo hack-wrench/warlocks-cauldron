e045e113c886080e8d53054e500f6aaff1c940ba
HentaiStorm <60922927+hack-wrench@users.noreply.github.com>
2023-01-12 18:25:12 +0300

    add simple provider option

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,7 @@
 use warlocks_cauldron::*;
 
 fn main() {
+    // A common option for most providers
     let complex = ComplexProvider::new(Locale::EN);
     println!("Person: {}", complex.person.full_name(None, false));
     println!("Telephone: {}", complex.person.telephone(None));
@@ -8,4 +9,8 @@ fn main() {
     println!("Birthday: {}", Datetime::date(1940, 2000));
     println!("Weight: {} kg", Person::weight(30, 90));
     println!("Height: {} m", Person::height(1.5, 2.0));
+
+    // But you can also use single providers
+    let russian_person = Person(Locale::RU);
+    println!("Their Russian friend: {}", russian_person.full_name(None, false));
 }
